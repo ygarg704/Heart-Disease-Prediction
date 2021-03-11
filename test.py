79183e8e85f0fa663935d30432b4f7bd2d5f6152
ygarg704 <ygarg704@gmail.com>
2021-03-12 00:14:25 +0530

    f

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import requests
 
 data = {'data': "67,1,4,160,286,0,2,108,1,1.5,2,3,3"}
-r = requests.post("http://127.0.0.1:5000/predict", json=data)
+r = requests.post("https://heart-predict.herokuapp.com/predict", json=data)
 print('response from server:', r)
